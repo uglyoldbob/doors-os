a400b9b41b97ba37d68e21e6f084a448af02d5b5
Thomas Epperson <thomas.epperson@gmail.com>
2023-12-12 09:58:19 -0600

    Add dummy file for a fmc controller on the stm32 board.

---
+++ kernel/src/modules/memory/stm32f769_fmc.rs
@@ -0,0 +1,4 @@
+//! The flash memory controller for the stm32f769 processor.
+
+/// The registers for the peripheral.
+pub struct Registers {}
--- kernel/src/modules/mod.rs
+++ kernel/src/modules/mod.rs
@@ -1,3 +1,4 @@
 //! Kernel modules belong in this module.
 
+pub mod memory;
 pub mod video;
