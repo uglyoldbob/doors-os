7194b0aa46b1ee231adda22fe52eb570897291f2
Thomas Epperson <thomas.epperson@gmail.com>
2024-04-23 16:48:06 -0500

    Start on building a font for the kernel.

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,11 +1,53 @@
 use std::io::Read;
 
+use ttf_parser::colr::Painter;
+
 use crate::config::KernelConfig;
 
 mod config;
 
+struct BitmapPainter {}
+
+impl BitmapPainter {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Painter for BitmapPainter {
+    fn outline(&mut self, glyph_id: ttf_parser::GlyphId) {
+        todo!()
+    }
+
+    fn paint_foreground(&mut self) {
+        todo!()
+    }
+
+    fn paint_color(&mut self, color: ttf_parser::RgbaColor) {
+        todo!()
+    }
+}
+
 fn main() {
     let target = target_build_utils::TargetInfo::new().expect("could not get target info");
+
+    println!("cargo:rerun-if-changed=./src/cmunbtl.ttf");
+    let font = include_bytes!("./src/cmunbtl.ttf");
+    let font = ttf_parser::Face::parse(font, 0).unwrap();
+    if font.is_variable() {
+        panic!("Not a fixed width font");
+    }
+    let mut font_bitmap = Vec::new();
+    let mut painter = BitmapPainter::new();
+    for i in 0..=255 {
+        let c = i as u8 as char;
+        if let Some(glyph) = font.glyph_index(c) {
+            font.paint_color_glyph(glyph, 0, &mut painter);
+            font_bitmap.push(1);
+        } else {
+            font_bitmap.push(0);
+        }
+    }
 
     let p = std::path::PathBuf::from("./config.toml");
     let mut config = std::fs::File::open(p).expect("Failed to open kernel configuration");
