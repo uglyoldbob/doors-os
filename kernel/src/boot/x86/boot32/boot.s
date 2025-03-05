c3b759c9bcd4da3e5026b3126be77bfcafe7319f
Thomas Epperson <thomas.epperson@gmail.com>
2025-03-04 23:03:01 -0600

    Fix name of startup function for x86 32-bit kernel.

--- kernel/src/boot/x86/boot32/boot.s
+++ kernel/src/boot/x86/boot32/boot.s
@@ -1,5 +1,5 @@
     .section .data
-    .global start
+    .global _start
     .extern MULTIBOOT2_DATA
     .extern INITIAL_STACK
     .extern start32
@@ -8,7 +8,7 @@
     INITIAL_STACK: .word 0
     .section .text
     .code32
-    start:
+    _start:
         mov [MULTIBOOT2_DATA], ebx
         mov [INITIAL_STACK], esp
         jmp start32
